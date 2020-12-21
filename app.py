ff56893de88b88daa5cdfdb13f5e3422cab4ee20
DougSherlock <dougthefiddler@hotmail.com>
2020-12-20 19:40:34 -0800

    First commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# Reference: https://www.youtube.com/watch?v=kqtD5dpn9C8
+# I paused at 15:06 in the video
+
+# age = 20
+# price = 19.5
+# first_name = "Doug"
+# is_online = False
+# print(age)
+
+# name = input("What is your name? ")
+# print("Hello " + nameDoug)
+
+# birth_year = input("What is your birth year? ")
+# age = 2020 - int(birth_year)
+# print('You are ' + str(age) + ' year old')
+
+first = float(input("First: "))
+second = float(input("Second: "))
+print("Sum: " + str(first + second))
